b28f137e17b6b3b02737ba345579a2fe64f6abf0
Aliaksei Raspiareza <rasp@mail.desy.de>
2023-12-13 20:55:15 +0100

    remove CombineCards.bash

--- combine/CombineCards.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-masses=(220 240 260 280 300 350 400)
-folder=${CMSSW_BASE}/src/AZh/combine/HIG-18-023/datacards
-cd ${folder}
-for mass in ${masses[@]}
-do
-    combineCards.py MMTT/Aconstr_HsvFit90/AZH${mass}/SR.card.txt MMMT/Aconstr_HsvFit90/AZH${mass}/SR.card.txt MMET/Aconstr_HsvFit90/AZH${mass}/SR.card.txt MMEM/Aconstr_HsvFit90/AZH${mass}/SR.card.txt EETT/Aconstr_HsvFit90/AZH${mass}/SR.card.txt EEMT/Aconstr_HsvFit90/AZH${mass}/SR.card.txt EEET/Aconstr_HsvFit90/AZH${mass}/SR.card.txt EEEM/Aconstr_HsvFit90/AZH${mass}/SR.card.txt > AZH${mass}.txt
-done
-cd -
