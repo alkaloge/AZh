c7419e931a5f7bf16beec20c50f64bf557c251e4
Alexei Raspereza <rasp@mail.desy.de>
2023-12-23 17:17:54 +0100

    Running closure

--- combine/RunGoF_Closure.bash
+++ combine/RunGoF_Closure.bash
@@ -1,17 +1,13 @@
 #!/bin/bash
 sample=$1
 folder=${CMSSW_BASE}/src/AZh/combine/ClosureTest/${sample}
-jobdir=${CMSSW_BASE}/src/AZh/combine/jobs
-#outdir=$2
-#if [ ! -d "$outdir" ]; then
-#    mkdir $outdir
-#else 
-cd GoF
-combineTool.py -M GoodnessOfFit -d ${folder}/ws.root  -m 300 --algo saturated -n .$sample
-for i in {1..2}
+mkdir GoF_ClosureTest_${sample}
+cd GoF_ClosureTest_${sample}
+combineTool.py -M GoodnessOfFit -d ${folder}/ws.root --fixedSignalStrength -m 300 --algo saturated -n .obs
+for i in {1..100}
 do
     random=$RANDOM
     echo random seed $random
-    combineTool.py -M GoodnessOfFit -d ${folder}/ws.root --toysFreq -m 300 --algo saturated -n .test_exp -t 10 -s ${random} --setParameters r_ggA=1,r_bbA=0 --job-mode condor --task-name gof.${random} --sub-opts='+JobFlavour = "workday"' 
+    combineTool.py -M GoodnessOfFit -d ${folder}/ws.root --toysFreq -m 300 --algo saturated -n .exp -t 10 -s ${random} --fixedSignalStrength 0  --job-mode condor --task-name gof.${random} --sub-opts='+JobFlavour = "workday"' 
 done
 cd -
