5ced1b07ffe4267c191a8602c7c633a620606dfa
Aliaksei Raspiareza <rasp@mail.desy.de>
2023-12-24 12:32:15 +0100

    removing Hadd_GoF_Closure.bash

--- combine/Hadd_GoF_Closure.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# $1 : sample name 
-# available options : 2016, 2017, 2018, em, et, mt, tt  
-cd GoF_ClosureTest_${sample}
-mv higgsCombine.obs.GoodnessOfFit.mH300.root gof_obs.root
-hadd gof_exp.root higgsCombine.exp.GoodnessOfFit.*.root 
-cd -
