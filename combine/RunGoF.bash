91c3cd7c683ac65f0ba5ab6703f89679bc2dafeb
Aliaksei Raspiareza <rasp@mail.desy.de>
2024-03-28 15:02:48 +0100

    RunGoF

--- combine/RunGoF.bash
+++ combine/RunGoF.bash
@@ -1,22 +1,21 @@
 #!/bin/bash
-##########################################################
-# definition of parameters to steer running of GoF tests #
-##########################################################
-folder=$1      # folder with datacards (e.g. datacards)
-sample=$2      # options : 2016, 2017, 2018, Run2, et, mt, tt
-mA=300         # mass hypothesis
-r_ggA=0        # signal strength of ggA process
-r_bbA=0        # signal strength of bbA process
-algo=saturated # test-statistics, options saturated, KS, AD 
-njobs=25       # number of jobs 
-ntoys=40       # number of toys per job
+################################################################
+#    definition of parameters to steer running of GoF tests    #
+################################################################
+sample=$1        # options : 2016, 2017, 2018, Run2, et, mt, tt
+mA=300           # mass hypothesis
+r_ggA=0          # signal strength of ggA process
+r_bbA=0          # signal strength of bbA process
+algo=saturated   # test-statistics, options saturated, KS, AD 
+njobs=25         # number of jobs 
+ntoys=40         # number of toys per job
+folder=datacards # folder with datacards (e.g. datacards)
 
 npar=$#
-if [ $npar -ne 2 ]; then
+if [ $npar -ne 1 ]; then
     echo 
-    echo Execute script with three parameters 
-    echo first argument : folder with datacards
-    echo second argument : 2016, 2017, 2018, em, et, mt, tt and Run2
+    echo Execute script with one parameter:
+    echo 2016, 2017, 2018, em, et, mt, tt and Run2
     echo Examples :
     echo ./RunGoF.bash datacards Run2
     echo ./RunGoF.bash datacards et
@@ -26,7 +25,7 @@ fi
 
 
 folder=${CMSSW_BASE}/src/AZh/combine/${folder}/${sample}/${mA} # folder with workspace
-outdir=${CMSSW_BASE}/src/AZh/combine/GoF_${folder}_${sample}_${mA} # output folder
+outdir=${CMSSW_BASE}/src/AZh/combine/GoF_${sample}_${mA} # output folder
 
 if [ ! -d "$outdir" ]; then
     echo creating folder ${outdir}
