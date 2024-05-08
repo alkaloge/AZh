def70d7870f7d8081470624a24d2f4eb6fc9994d
Aliaksei Raspiareza <rasp@mail.desy.de>
2024-05-08 12:17:21 +0200

    cleaning repo

--- combine/2Dscan_300/Run2D_scan.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ulimit -s unlimited
-combineTool.py -M MultiDimFit -m 300 --setParameterRanges r_bbA=-2,5:r_ggA=-5,5 ws.root --algo=grid --points=10000 -t -1 --setParameters r_bbA=0,r_ggA=0 --robustFit 1 --cminDefaultMinimizerTolerance 0.01 --cminDefaultMinimizerStrategy=0 --X-rtd MINIMIZER_analytic -n .2Dscan -v2  
